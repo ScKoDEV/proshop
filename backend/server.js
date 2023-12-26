ad952505dafabb2435669e098bf97881a0f0d1f0
Konstantin Schiller <ks@schillerwebsolutions.com>
2023-12-26 08:00:18 -0300

    added env file and variables

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import products from './data/products.js'
+import dotenv from 'dotenv'
+dotenv.config();
 
-const port = 5001;
+const port = process.env.PORT || 5001;
 
 const app = express();
 
