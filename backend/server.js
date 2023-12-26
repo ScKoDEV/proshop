7a9538bfd407685a82506b29f60a197f8b61f774
Konstantin Schiller <ks@schillerwebsolutions.com>
2023-12-26 08:30:22 -0300

    fetching products from the backend with axios usestate and useeffect

--- backend/server.js
+++ backend/server.js
@@ -10,10 +10,10 @@ const app = express();
 app.get('/' , (req, res) => { 
     res.send('API is running...');
  })
- app.get('/products' , (req, res) => { 
+ app.get('/api/products' , (req, res) => { 
     res.json(products);
  })
- app.get('/products/:id' , (req, res) => { 
+ app.get('/api/products/:id' , (req, res) => { 
     const product = products.find((p) => p._id === req.params.id);
     res.json(product);
  })
