0a904c09fed9336714675f94d88b6559610de696
Konstantin Schiller <ks@schillerwebsolutions.com>
2024-01-08 08:35:57 -0300

    create order page

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -54,7 +54,7 @@ const getOrderById = asyncHandler(async (req, res) => {
     const order = await Order.findById(req.params.id).populate('user', 'name email')
 
     if(order){
-        res.stauts(200).json(order)
+        res.status(200).json(order)
     } else {
         res.status(404)
         throw new Error('Order not found')
