8adfee5bbcebf9bb05c8244d7e7540831941b14f
Konstantin Schiller <ks@schillerwebsolutions.com>
2024-01-10 08:01:49 -0300

    added paypal as payment method

--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -1,15 +1,45 @@
-import React from 'react'
+import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
 import {useParams, Link} from 'react-router-dom'
 import {Row, Col, ListGroup, Image, Form, Button, Card} from 'react-bootstrap'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
-import { useGetOrderDetailsQuery } from '../slices/ordersApiSlice'
+import { useGetOrderDetailsQuery, useGetPayPalClientIdQuery, usePayOrderMutation } from '../slices/ordersApiSlice'
+import {PayPalButtons, usePayPalScriptReducer} from '@paypal/react-paypal-js'
+import { toast } from 'react-toastify'
 
 
 const OrderScreen = () => {
 
     const {id: orderId} = useParams()
     const {data: order, refetch, isLoading, error} = useGetOrderDetailsQuery(orderId)
+
+    const [payOrder, {isLoading: loadingPay}] = usePayOrderMutation()
+    const [{isPending}, paypalDispatch] = usePayPalScriptReducer()
+
+    const {data: paypal, isLoading: loadingPayPal, error: errorPayPal} = useGetPayPalClientIdQuery()
+    
+    const {userInfo} = useSelector((state) => state.auth)
+
+    useEffect(() => {
+        if(!errorPayPal && !loadingPayPal && paypal.clientId){
+            const loadPayPalScript = async () => {
+                paypalDispatch({
+                    type: 'resetOptions',
+                    value: {
+                        'client-id': paypal.clientId,
+                        currency: 'USD'
+                    }
+                })
+                paypalDispatch({type: 'setLoadingStatus', value: 'pending'})
+            }
+            if (order && !order.isPaid){
+                if(!window.paypal){
+                    loadPayPalScript()
+                }
+            }
+        }
+    }, [])
 
   return isLoading ? <Loader /> : error ? <Message variant="danger" /> : (
     <>
