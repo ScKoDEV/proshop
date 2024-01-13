45db55a1cf0aa77fc1d1e10296ef1a77fd6e07f8
Konstantin Schiller <ks@schillerwebsolutions.com>
2024-01-13 14:57:51 -0300

    Admin Route Component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -20,6 +20,8 @@ import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
 import {PayPalScriptProvider} from '@paypal/react-paypal-js'
 import ProfileScreen from './screens/ProfileScreen';
+import AdminRoute from './components/AdminRoute';
+import OrderListScreen from './screens/OrderListScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -36,6 +38,10 @@ const router = createBrowserRouter(
               <Route path="/placeorder" element={<PlaceOrderScreen/>} />
               <Route path="/order/:id" element={<OrderScreen/>} />
               <Route path="/profile" element={<ProfileScreen/>} />
+      </Route>
+
+      <Route path='' element={<AdminRoute />}>
+              <Route path="/admin/orderlist" element={<OrderListScreen/>} />
       </Route>
     </Route>
   )
