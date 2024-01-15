67acdcd575db6277dad384cc4f7be45f66cc282b
Konstantin Schiller <ks@schillerwebsolutions.com>
2024-01-14 21:56:54 -0300

    Creating Products

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getProductById, getProducts } from "../controllers/productController.js";
+import { createProduct, getProductById, getProducts } from "../controllers/productController.js";
+import {protect, admin} from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
-router.route('/').get(getProducts)
+router.route('/').get(getProducts).post(protect, admin, createProduct)
 router.route('/:id').get(getProductById)
 
 export default router
