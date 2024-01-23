f5d30bad78e59cb21cce86c507713fa371c363b3
Konstantin Schiller <ks@schillerwebsolutions.com>
2024-01-22 22:32:24 -0300

    Setting up for production

--- backend/server.js
+++ backend/server.js
@@ -23,9 +23,7 @@ app.use(express.urlencoded({extended: true}))
 // Cookie parser middleware
 app.use(cookieParser())
 
-app.get('/' , (req, res) => { 
-    res.send('API is running...');
- })
+
  
  app.use('/api/products', productRoutes)
  app.use('/api/users', userRoutes)
@@ -36,6 +34,18 @@ app.get('/' , (req, res) => {
 
  const __dirname = path.resolve() // set __dirname to current directory
  app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+ if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+    // any route that is not api will be redirected to index.html
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+ } else {
+    app.get('/' , (req, res) => { 
+        res.send('API is running...');
+     })
+ }
 
  app.use(notFound)
  app.use(errorHandler)
