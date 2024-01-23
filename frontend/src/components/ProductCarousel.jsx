fee1ec01c6c7648ded19aa1400e34b944614e192
Konstantin Schiller <ks@schillerwebsolutions.com>
2024-01-22 22:43:17 -0300

    removed second loader

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import {Carousel, Image} from 'react-bootstrap'
-import Loader from './Loader'
 import Message from './Message'
 import { useGetTopProductsQuery } from '../slices/productsApiSlice'
 
 const ProductCarousel = () => {
-    const {data: products, isLoading, error} = useGetTopProductsQuery()
-  return isLoading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
+    const {data: products, error} = useGetTopProductsQuery()
+  return error ? <Message variant='danger'>{error}</Message> : (
     <Carousel pause='hover' className='bg-primary mb-4'>
       {products.map(product => (
         <Carousel.Item key={product._id}>
